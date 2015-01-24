dd16c91cefce78dffff6859ddd658ced4630a8ad
khp <kanghee.p@gmail.com>
2015-01-24 00:56:51 -0800

    Changed Map.java

--- core/src/io/khp/github/Map.java
+++ core/src/io/khp/github/Map.java
@@ -5,13 +5,30 @@ import java.util.ArrayList;
 public class Map {
 	
 	private ArrayList<Tile> tileArray;
-	private int tileHeight = 10;
-	private int tileWidth = 10;
+	private int tileHeight;
+	private int tileWidth;
 
 	Map (int pxWidth, int pxHeight, int pxTileWidth, int pxTileHeight) {
-		tileHeight = pxHeight/pxTileHeight;
-		tileWidth = pxWidth/pxTileWidth;
+		this.tileHeight = pxHeight/pxTileHeight;
+		this.tileWidth = pxWidth/pxTileWidth;
+		this.tileArray = new ArrayList();
+		
 	}
+	
+	Map () {
+		tileArray = new ArrayList();
+		tileWidth = 24;
+		tileHeight = 40;
+		for (int i = 0; i < 24 * 40; i++) {
+			tileArray.add(new Tile(TileType.EMPTY, 0, 0));
+		}
+		tileArray.get(24*40-1).setType(TileType.WALL);
+		tileArray.get(24*40-2).setType(TileType.WALL);
+	}
+	
+	
+	
+	
 	
 	
 }
