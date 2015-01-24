3b077052fc280c6dc53c5bdf33b0707225b9ac1d
tlin23 <tlin23@gmail.com>
2015-01-23 22:49:27 -0800

    Updated main and player1 for ryan ;)

--- core/src/io/khp/github/Player1.java
+++ core/src/io/khp/github/Player1.java
@@ -1,5 +1,26 @@
 package io.khp.github;
 
-public class Player1 {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.*;
 
+public class Player1 {
+	private Rectangle rect;
+	
+	public void Player1() {
+		rect = new Rectangle();
+		rect.x = 800 / 2 - 64 / 2;
+		rect.y = 20;
+		rect.width = 64;
+		rect.height = 64;
+	}
+	
+	public void moveLeft() {
+		rect.x -= 200 * Gdx.graphics.getDeltaTime();
+	}
+	
+	public void moveRight() {
+		rect.x += 200 * Gdx.graphics.getDeltaTime();
+	}
+	
+	public 
 }
