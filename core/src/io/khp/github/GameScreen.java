0870e64797336d078200f0e53901b8608dacf5f6
HeroNim <alexradvenis@gmail.com>
2015-01-24 18:57:22 -0800

    cleanup

--- core/src/io/khp/github/GameScreen.java
+++ core/src/io/khp/github/GameScreen.java
@@ -1,27 +1,15 @@
 package io.khp.github;
 
-import com.badlogic.gdx.Game;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
@@ -49,11 +37,8 @@ public class GameScreen implements Screen{
 	
 	private FileHandle testMap;
 	private MapDrawer mapDrawer;
-	private JumpRenew jumprenew;
 	private JumpRenew[] renewButtons;
 	
-	private Rectangle intersectionPlayer1;
-	private Rectangle intersectionPlayer2;
 	private Rectangle intersectionPlayers;
 	
 	private Map map;
@@ -83,8 +68,6 @@ public class GameScreen implements Screen{
 			players[0] = player1;
 			players[1] = player2;
 			
-			intersectionPlayer1 = new Rectangle();
-			intersectionPlayer2 = new Rectangle();
 			intersectionPlayers = new Rectangle();
 			
 			// set up the jumprenews
